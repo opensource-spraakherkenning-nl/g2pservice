791edb57e717a6c1a5b84ece15a8e8c43d3a2707
Maarten van Gompel <proycon@anaproy.nl>
2019-11-20 17:23:16 +0100

    removed the needless latin1->utf-8 conversion. input was already marked as utf-8

--- g2pservice/g2pservice_wrapper.py
+++ g2pservice/g2pservice_wrapper.py
@@ -106,14 +106,13 @@ for inputfile in clamdata.input:
     inputfilepath = str(inputfile)
     if inputtemplate == 'wordlist':
         basename = os.path.join(outputdir, os.path.basename(inputfilepath)[:-4])
-        inputfile_utf8 = basename + ".utf8"
+        inputfile = basename + ".utf8"
         gtp_in = basename + ".gtp_in"
         gtp_table = basename + ".word_table"
         phonetisaurusoutput = basename + ".gtp_out"
         outputfile = basename + ".dict"
-        os.system("iconv -f ISO-8859-1 -t utf8 " + shellsafe(inputfilepath,'"') + " > " + shellsafe(inputfile_utf8,'"'))
-        os.system("cat " + shellsafe(inputfile_utf8,'"') + " | perl " + os.path.join(basedir,"flatten.perl") + " 0 > " + shellsafe(gtp_in,'"'))
-        os.system("cat " + shellsafe(inputfile_utf8,'"') + " | perl " + os.path.join(basedir,"flatten.perl") + " 1 > " + shellsafe(gtp_table,'"'))
+        os.system("cat " + shellsafe(inputfile,'"') + " | perl " + os.path.join(basedir,"flatten.perl") + " 0 > " + shellsafe(gtp_in,'"'))
+        os.system("cat " + shellsafe(inputfile,'"') + " | perl " + os.path.join(basedir,"flatten.perl") + " 1 > " + shellsafe(gtp_table,'"'))
         if os.system("phonetisaurus-apply --model " + shellsafe( os.path.join(basedir, modelname),'"')+ " --word_list " + shellsafe(gtp_in,'"') + " -n " + str(clamdata['n']) + " > " + shellsafe(phonetisaurusoutput,'"')) != 0:
             clam.common.status.write(statusfile, "Error calling phonetisaurus",100) # status update
             sys.exit(1)
